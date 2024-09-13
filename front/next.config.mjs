8c8ec5c97b38e20227376d286a14b30b38ffa4c8
kimgangmoon <162942474+web5-kimgangmoon@users.noreply.github.com>
2024-09-14 06:10:29 +0900

    fix:addBasePath

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -9,9 +9,7 @@ const nextConfig = {
   // skipTrailingSlashRedirect: true,
   // Optional: Change the output directory `out` -> `dist`
   // distDir: 'dist',
-  images: {
-    domains: ["127.0.0.1"],
-  },
+  basePath: "/",
 };
 
 export default nextConfig;
