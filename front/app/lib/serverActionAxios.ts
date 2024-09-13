483c1a7e85aa22ee257fdc2304743519df78fd3c
kimgangmoon <162942474+web5-kimgangmoon@users.noreply.github.com>
2024-09-14 06:32:46 +0900

    fix:path

--- front/app/lib/serverActionAxios.ts
+++ front/app/lib/serverActionAxios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const serverAxios = axios.create({
-  baseURL: "http://localhost:3080/api",
+  baseURL: "/api",
 });
 
 export default serverAxios;
--- front/next.config.mjs
+++ front/next.config.mjs
@@ -9,7 +9,6 @@ const nextConfig = {
   // skipTrailingSlashRedirect: true,
   // Optional: Change the output directory `out` -> `dist`
   // distDir: 'dist',
-  basePath: "/",
 };
 
 export default nextConfig;
