de0c8c2f1365bedd2305c8bca92d7354a5a25b37
kimgangmoon <162942474+web5-kimgangmoon@users.noreply.github.com>
2024-09-14 09:37:26 +0900

    feat:formatNremotePatterns

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -2,7 +2,11 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  images: { domains: ["localhost", "/api", "52.79.197.169"] },
+  images: {
+    domains: ["localhost", "/api", "52.79.197.169"],
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [{ hostname: "/api" }],
+  },
 };
 
 export default nextConfig;
