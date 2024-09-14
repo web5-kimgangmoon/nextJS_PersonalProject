b14f137e27c903c048b65f741780a0a0d0d3b3d8
kimgangmoon <162942474+web5-kimgangmoon@users.noreply.github.com>
2024-09-14 15:14:28 +0900

    fix:envRoot

--- front/app/lib/serverActionAxios.ts
+++ front/app/lib/serverActionAxios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const serverAxios = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_SERVER_PATH}`,
+  baseURL: process.env.NEXT_PUBLIC_SERVER_PATH,
 });
 
 export default serverAxios;
