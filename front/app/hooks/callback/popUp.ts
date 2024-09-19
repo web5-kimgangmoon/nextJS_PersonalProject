0b3ef186f929f464d4bbdf69e825866f5f4d876b
kimgangmoon <162942474+web5-kimgangmoon@users.noreply.github.com>
2024-09-20 00:44:11 +0900

    fix:error

--- front/app/hooks/callback/popUp.ts
+++ front/app/hooks/callback/popUp.ts
@@ -1,10 +1,10 @@
 import { useCallback } from "react";
-import { oauthLogin } from "@/app/lib/actions";
+// import { oauthLogin } from "@/app/lib/actions";
 export const usePopup = (address: string, oauth: "google" | "facebook") =>
   useCallback(() => {
     const s = window.open(address, "popupWindow", "width=600px,height=400px");
     window.onmessage = function (e: MessageEvent) {
-      oauthLogin(e.data, oauth);
+      // oauthLogin(e.data, oauth);
       s?.close();
     };
     window.onmessageerror = (e) => {
